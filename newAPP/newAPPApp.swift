621960a5385088454d327418088c9da8e0f988e4
abbbrr <ruanablaikz45@gmail.com>
2023-10-13 10:40:10 +0600

    Initial Commit

---
+++ newAPP/newAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAPPApp.swift
+//  newAPP
+//
+//  Created by Рауан Аблайхан on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct newAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
